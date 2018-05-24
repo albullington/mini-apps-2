9092cff3bf917550d6ba0dec37051a6ca7676c30
Amanda <albullington@gmail.com>
2018-05-24 10:42:58 -0700

    express server created

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,14 @@
+const express = require('express');
+const path = require('path');
 
+const app = express();
 
+app.use(express.static(path.join(__dirname) + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendStatus(200).send('Hello world');
+})
+
+const port = 3001;
+
+app.listen(port, () => console.log(`listening on http://localhost:${port}`));
