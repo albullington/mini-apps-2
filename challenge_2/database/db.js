b16249cb85ea2e0034928d9d638c30bd365aab82
Amanda <albullington@gmail.com>
2018-05-24 22:56:19 -0700

    set up database schema and tables

--- challenge_2/database/db.js
+++ challenge_2/database/db.js
@@ -1,10 +1,36 @@
-const { Pool, Client } = require('pg');
-const client = new Client();
+const knex = require('knex')({
+  client: 'postgres',
+  connection: {
+    host: '127.0.0.1',
+    user: 'Amanda',
+    password: 'password',
+    database: 'Amanda',
+  },
+});
 
-await client.connect();
-
-const res = await client.query('SELECT $1::text as message', ['Hello world!']);
-
-console.log(res.rows[0].message)
-
-await client.end();
+knex.schema.createTable('users', (table) => {
+  table.increments('id');
+  table.string('name');
+  table.string('email');
+  table.string('password');
+}).createTable('addresses', (table) => {
+  table.increments('id');
+  table.string('line1');
+  table.string('line2');
+  table.string('city');
+  table.string('state');
+  table.string('zipcode');
+  table.integer('user_id').unsigned().references('users.id');
+}).createTable('payments', (table) => {
+  table.increments('id');
+  table.string('creditcard');
+  table.string('expirydate');
+  table.string('cvv');
+  table.string('billingzip');
+  table.integer('user_id').unsigned().references('users.id');
+}).then(() => {
+  return knex.insert({ name: 'Amanda' }).into('users');
+})
+  .catch((e) => {
+    console.log(e);
+  });
