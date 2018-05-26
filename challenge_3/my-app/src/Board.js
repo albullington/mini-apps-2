e03f26aa34addd1ce7d184db73821dfdc237cb2e
Amanda <albullington@gmail.com>
2018-05-26 15:53:45 -0700

    moved grid function out of render

--- challenge_3/my-app/src/Board.js
+++ challenge_3/my-app/src/Board.js
@@ -2,16 +2,40 @@ import React, { Component } from 'react';
 import Square from './Square.js';
 
 class Board extends Component {
+  constructor() {
+    super();
 
-  render() {
+    this.state = {
+      gameWon: false,
+      minesPlanted: 0,
+      rows: this.createGrid(),
+    }
+  }
+
+  createGrid() {
     let rows = [];
     for (let y = 0; y < 10; y++) {
       const squares = [];
       for (let x = 0; x < 10; x++) {
-        squares.push(<Square key={`square-col${x}-row${y}`} value={`square-col${x}-row${y}`}/>);
+        squares.push(<Square 
+          key={`square-col${x}-row${y}`}
+          isEmpty="false" 
+          isBomb="false"
+          numAdjacent="0"
+          isShown="false"
+          value={`square-col${x}-row${y}`}
+        />);
       }
       rows.push(<tr key={y}>{squares}</tr>);
     }
+    return rows;
+  }
+
+  render() {
+    const {
+      rows
+    } = this.state;
+
     return (
       <table>
         <tbody>
